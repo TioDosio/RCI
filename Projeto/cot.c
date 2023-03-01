a51071320f81cb51a4d88b57b15fb188bc34fa3f
Andre <andre.teodosio@tecnico.ulisboa.pt>
2023-03-01 22:58:27 +0000

    argv de entrada c/ erros

--- Projeto/cot.c
+++ Projeto/cot.c
@@ -9,7 +9,22 @@ int main(int argc, char *argv[])
         break;
 
     case 2:
-        char arg = argv[1];
-        printf("Argumento: %s\n", arg);
+        char regIP = "193.136.138.142";
+        int regUDP = 59000;
+        printf("RegIp: %s\n", regIP);
+        printf("RegUDP: %d\n", regUDP);
+        IP = argc[1];
+        TCP = argc[2];
+        printf("IP: %s\n", IP);
+        printf("TCP: %d\n", TCP);
+    case 4:
+        char regIP;
+        int regUDP;
+        printf("RegIp: %s\n", regIP);
+        printf("RegUDP: %d\n", regUDP);
+        IP = argc[1];
+        TCP = argc[2];
+        printf("IP: %s\n", IP);
+        printf("TCP: %d\n", TCP);
     }
 }
