c10a87cd0b4b22b5ba481ed30f02f0ce9617da5d
Andre <andre.teodosio@tecnico.ulisboa.pt>
2023-03-03 19:04:55 +0000

    algus erros resolvidos argv

--- Projeto/cot.c
+++ Projeto/cot.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char argv[])
 {
+    char IP;
+    char TCP;
+    char regIP;
+    int regUDP;
     switch (argc)
     {
     case 1:
@@ -9,21 +13,20 @@ int main(int argc, char *argv[])
         break;
 
     case 2:
-        char regIP = "193.136.138.142";
-        int regUDP = 59000;
+        regIP = "193.136.138.142";
+        regUDP = 59000;
         printf("RegIp: %s\n", regIP);
         printf("RegUDP: %d\n", regUDP);
-        IP = argc[1];
-        TCP = argc[2];
+        IP = argv[1];
+        TCP = argv[2];
         printf("IP: %s\n", IP);
         printf("TCP: %d\n", TCP);
     case 4:
-        char regIP;
-        int regUDP;
+
         printf("RegIp: %s\n", regIP);
         printf("RegUDP: %d\n", regUDP);
-        IP = argc[1];
-        TCP = argc[2];
+        IP = argv[1];
+        TCP = arg[2];
         printf("IP: %s\n", IP);
         printf("TCP: %d\n", TCP);
     }
