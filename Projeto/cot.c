8ada2d739dc6926ffb19a4f28ce1f904c0058fbe
Andre <andre.teodosio@tecnico.ulisboa.pt>
2023-03-05 20:07:57 +0000

    Argumentos de entrada Completos :)

--- Projeto/cot.c
+++ Projeto/cot.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-    char IP;
-    char TCP;
-    char regIP;
-    int regUDP;
+    char *IP = NULL;    // IP do TCP que é dado
+    int TCP = 0;        // Porto do TCP que é dado
+    char *regIP = NULL; // IP do UDP pode ou não ser dado
+    int regUDP = 0;     // Porto do UDP que pode ou não ser dado
     switch (argc)
     {
     case 1:
-        printf("Sem argumentos\n");
+        printf("Sem argumentos\n"); // Não tem argumentos
         break;
-
-    case 2:
+    case 3: // Recebe apenas o IP e o TCP
+        printf("Com 2 argumentos\n");
         regIP = "193.136.138.142";
         regUDP = 59000;
-        printf("RegIp: %s\n", regIP);
-        printf("RegUDP: %d\n", regUDP);
         IP = argv[1];
-        TCP = argv[2];
+        TCP = atoi(argv[2]);
         printf("IP: %s\n", IP);
         printf("TCP: %d\n", TCP);
-    case 4:
-
         printf("RegIp: %s\n", regIP);
         printf("RegUDP: %d\n", regUDP);
+        break;
+    case 5: // Recebe o IP e o TCP e o IP e o UDP
+        printf("Com 4 argumentos\n");
         IP = argv[1];
-        TCP = arg[2];
+        regIP = argv[3];
+        TCP = atoi(argv[2]);
+        regUDP = atoi(argv[4]);
         printf("IP: %s\n", IP);
         printf("TCP: %d\n", TCP);
+        printf("RegIp: %s\n", regIP);
+        printf("RegUDP: %d\n", regUDP);
+        break;
+    default:
+        printf("Argumentos invalidos\n");
+        printf("argc %d\n", argc);
+        break;
     }
 }
